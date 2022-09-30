55bbcbf7555908bbc17b60f47bbb4a7251284ded
Steven Lu (5950X 20.04[tmux]) <stevenlu443@gmail.com>
2022-09-30 00:58:54 -0400

    - old bugs fixed - new bugs added

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import validator from 'validator'
-import EventSource from 'eventsource'
-import superagent from 'superagent'
-import url from 'url'
-import querystring from 'querystring'
+import validator from "validator";
+import EventSource from "eventsource";
+import superagent from "superagent";
+import url from "url";
+import querystring from "querystring";
 
-type Severity = 'info' | 'error'
+type Severity = "info" | "error";
 
 interface Options {
-  source: string
-  target: string
-  logger?: Pick<Console, Severity>
+  source: string;
+  target: string;
+  logger?: Pick<Console, Severity>;
 }
 
 class Client {
@@ -18,71 +18,76 @@ class Client {
   logger: Pick<Console, Severity>;
   events!: EventSource;
 
-  constructor ({ source, target, logger = console }: Options) {
-    this.source = source
-    this.target = target
-    this.logger = logger!
+  constructor({ source, target, logger = console }: Options) {
+    this.source = source;
+    this.target = target;
+    this.logger = logger!;
 
     if (!validator.isURL(this.source)) {
-      throw new Error('The provided URL is invalid.')
+      throw new Error("The provided URL is invalid.");
     }
   }
 
-  static async createChannel () {
-    return superagent.head('https://smee.io/new').redirects(0).catch((err) => {
-      return err.response.headers.location
-    })
+  static async createChannel() {
+    return superagent
+      .head("https://smee.io/new")
+      .redirects(0)
+      .catch((err) => {
+        return err.response.headers.location;
+      });
   }
 
-  onmessage (msg: any) {
-    const data = JSON.parse(msg.data)
+  onmessage(msg: any) {
+    const data = JSON.parse(msg.data);
 
-    const target = url.parse(this.target, true)
-    const mergedQuery = Object.assign(target.query, data.query)
-    target.search = querystring.stringify(mergedQuery)
+    const target = url.parse(this.target, true);
+    const mergedQuery = Object.assign(target.query, data.query);
+    target.search = querystring.stringify(mergedQuery);
 
-    delete data.query
+    this.logger.info(data.query);
+    delete data.query;
 
-    const req = superagent.post(url.format(target)).send(data.body)
+    const req = superagent.post(url.format(target)).send(data.body);
 
-    delete data.body
+    this.logger.info(data.body);
+    delete data.body;
 
-    Object.keys(data).forEach(key => {
-      req.set(key, data[key])
-    })
+    Object.keys(data).forEach((key) => {
+      req.set(key, data[key]);
+    });
 
     req.end((err, res) => {
       if (err) {
-        this.logger.error(err)
+        this.logger.error(err);
       } else {
-        this.logger.info(`${req.method} ${req.url} - ${res.status}`)
+        this.logger.info(`${req.method} ${req.url} - ${res.status}`);
       }
-    })
+    });
   }
 
-  onopen () {
-    this.logger.info('Connected', this.events.url)
+  onopen() {
+    this.logger.info("Connected", this.events.url);
   }
 
-  onerror (err: any) {
-    this.logger.error(err)
+  onerror(err: any) {
+    this.logger.error(err);
   }
 
-  start () {
+  start() {
     const events = new EventSource(this.source);
 
     // Reconnect immediately
-    (events as any).reconnectInterval = 0 // This isn't a valid property of EventSource
+    (events as any).reconnectInterval = 0; // This isn't a valid property of EventSource
 
-    events.addEventListener('message', this.onmessage.bind(this))
-    events.addEventListener('open', this.onopen.bind(this))
-    events.addEventListener('error', this.onerror.bind(this))
+    events.addEventListener("message", this.onmessage.bind(this));
+    events.addEventListener("open", this.onopen.bind(this));
+    events.addEventListener("error", this.onerror.bind(this));
 
-    this.logger.info(`Forwarding ${this.source} to ${this.target}`)
-    this.events = events
+    this.logger.info(`Forwarding ${this.source} to ${this.target}`);
+    this.events = events;
 
-    return events
+    return events;
   }
 }
 
-export = Client
+export = Client;
